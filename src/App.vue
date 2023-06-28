be4987f84af2e82a2ff96c61bb572601e6c9126b
Stefano-Bernini <stefanobernini26@gmail.com>
2023-06-28 21:38:02 +0200

    boilerplate

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppCard from './components/AppCard.vue'
+
+export default {
+  components:{
+    AppHeader,
+    AppMain,
+    AppCard
+  }
+  
+}
+</script>
+
+<template>
+  <div>
+    <AppHeader />
+    <AppMain />
+    <AppCard />
+  </div>
+</template>
+
+<style lang="scss">
+  @use './styles/generals.scss' as *;
+</style>
